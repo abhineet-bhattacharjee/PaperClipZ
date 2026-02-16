a9e986afdd1a1931c4d05c805d6910f2cf2659c2
Abhineet Bhattacharjee <abhineetbhattacharjee@gmail.com>
2026-02-16 22:42:38 +0530

    chore: draw tray image

--- gui/tray.py
+++ gui/tray.py
@@ -5,3 +5,7 @@ from PIL import Image, ImageDraw
 def create_tray_icon():
     width = 64
     height = 64
+
+    image = Image.new('RGB', (width, height), (30, 144, 255))
+    dc = ImageDraw.Draw(image)
+
