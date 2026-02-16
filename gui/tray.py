7155afdbad229a91693a14bd46a6d8ed72c79653
Abhineet Bhattacharjee <abhineetbhattacharjee@gmail.com>
2026-02-16 22:43:25 +0530

    chore: add outlines to image

--- gui/tray.py
+++ gui/tray.py
@@ -9,3 +9,7 @@ def create_tray_icon():
     image = Image.new('RGB', (width, height), (30, 144, 255))
     dc = ImageDraw.Draw(image)
 
+    dc.rectangle([12, 16, 52, 48], fill='white', outline='black', width=2)
+    dc.rectangle([20, 8, 44, 20], fill='white', outline='black', width=2)
+
+    return image
