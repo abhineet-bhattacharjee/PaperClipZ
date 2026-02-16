29131fb4578f48b4d23bacd178799d7e3c454eaf
Abhineet Bhattacharjee <abhineetbhattacharjee@gmail.com>
2026-02-16 23:04:28 +0530

    chore: create geometry for main window

--- gui/main_window.py
+++ gui/main_window.py
@@ -3,3 +3,8 @@ import customtkinter as ctk
 class MainWindow(ctk.CTk):
     def __init__(self, paperclipz):
         super().__init__()
+
+        self.paperclipz = paperclipz
+
+        self.title("PaperClipZ")
+        self.geometry("800x600")
