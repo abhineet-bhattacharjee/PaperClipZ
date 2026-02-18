7f35eae1244d5e2324fd69f6a304c619b9b095a4
Abhineet Bhattacharjee <abhineetbhattacharjee@gmail.com>
2026-02-18 12:07:18 +0530

    feat: create class ClipboardCard

--- gui/clipboard_card.py
+++ gui/clipboard_card.py
@@ -1,3 +1,11 @@
 import customtkinter as ctk
 
 
+class ClipboardCard(ctk.CTkFrame):
+    def __init__(self, parent, text="Example Clipboard Text", **kwargs):
+        super().__init__(
+            parent,
+            corner_radius = 10,
+            fg_color=('gray90', 'gray13'),
+            **kwargs
+        )
